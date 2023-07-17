7770ac091eef7900372a8cf0c4e30cc8d9578fd9
Ferney Córdoba Canchala <fecork88@gmail.com>
2023-07-17 15:33:06 -0500

    🚧 build(startup.sh): lo movemos a /app

--- app/startup.sh
+++ app/startup.sh
@@ -1,4 +1,4 @@
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker app\main:app \
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app \
     --log-level=debug \
     --log-file=- \
     --access-logfile=-
