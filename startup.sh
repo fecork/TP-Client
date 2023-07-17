e4eebb9569e8d1ffb217733a583650106f097bfc
Ferney Córdoba Canchala <fecork88@gmail.com>
2023-07-17 14:48:41 -0500

    🚧 build(DEPLOY): startup

--- startup.sh
+++ startup.sh
@@ -1,1 +1,4 @@
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker app/main:app
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker app/main:app \
+    --log-level=debug \
+    --log-file=- \
+    --access-logfile=-
